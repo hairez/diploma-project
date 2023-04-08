b29b541a2a8221c714a6abf3418bbc13bf86f55e
hairez <hairez@github.com>
2023-04-08 17:36:12 +0200

    updated generator.sh

--- code/TSP/data/generator.sh
+++ code/TSP/data/generator.sh
@@ -5,36 +5,24 @@ PPATH=$(realpath ..)
 REQUIRE_SAMPLE_REUSE=0
 . ../../testdata_tools/gen.sh
 
-use_solution jonathan.cpp
+use_solution randomPath.py
 
 compile gen.py
 
-samplegroup
-sample 1
-sample 2
+group g1 10
+tc random_small gen n=10
 
-group g1 20
-limits maxn=2
-tc tinyn1 gen n=1
-tc tinyn2 gen n=2
-tc tinyn3 gen n=2
-tc tinyn4 gen n=2
+group g2 10
+tc random_medium gen n=1000
 
-group g2 20
-limits reversed=1
-tc reversed1 gen reversed=1 n=1000 max_ai=1000
-tc reversed2 gen reversed=1 n=10000 max_ai=1000
-tc reversed3 gen reversed=1 max_ai=1000
-tc reversed4 gen reversed=1
+group g3 10
+tc random_big gen n=1000000
 
-group g3 20
-limits maxn=1000
+group g4 10
 tc smalln1 gen n=1000
-tc smalln2 gen n=1000
-tc smalln3 gen n=1000
-tc smalln4 gen n=1000
 
-group g4 40
+
+group g5 40
 include_group g1
 include_group g2
 include_group sample
