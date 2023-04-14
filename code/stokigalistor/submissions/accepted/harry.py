4b92a569dbe16a068b640e65e8cbc6b79e0067db
hairez <75111093+hairez@users.noreply.github.com>
2023-04-14 12:57:32 +0200

    clean up

--- code/stokigalistor/submissions/accepted/harry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-n = int(input())
-
-list1 = list(map(int,input().split()))
-
-list2 = sorted(list1)
-
-print(sum(list1[i] != list2[i] for i in range(n)))
--- code/stokigalistor/submissions/accepted/jonathan.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-n = int(input())
-inp = list(map(int, input().split()))
-print(sum(a != b for a, b in zip(inp, sorted(inp))))
